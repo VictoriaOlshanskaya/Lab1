ef0525ff9043626aee020864af9e1573097aa0c2
User <User@WIN-9OL4CJ22DNO>
2023-11-06 21:52:16 +0100

    Добавьте файлы проекта.

---
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,36 @@
+﻿    #include <iostream>
+    #include <Windows.h>
+    #include <stdlib.h>
+int main()
+{
+    //Задача HELLO
+    std::cout << "HELLO!!!\n";
+
+    //Задача QUARE
+    std::cout << " _________\n";
+    std::cout << "|         |\n";
+    std::cout << "|         |\n";
+    std::cout << "|         |\n";
+    std::cout << "|         |\n";
+    std::cout << " ---------\n";
+
+    //Задача ANKETA
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::cout << "       Прізвище : Olshanska\n";
+    std::cout << "           Ім'я : Victoria\n";
+    std::cout << "    По батькові : Gennadivna\n";
+    std::cout << "          Стать : female\n";
+    std::cout << "Дата народження : 2006-01-07\n";
+    std::cout << "         Адреса : Presov, Slovakia\n";
+    std::cout << " Номер телефону : +380 96 379 7895\n";
+    std::cout << " Місце навчання : ФПМ, ДНУ ім. Олеся Гончара\n";
+    std::cout << "          Група : ПА-23-1\n";
+    std::cout << "          Хоббі : програмування\n";
+
+    system("pause");
+
+}
+
+
